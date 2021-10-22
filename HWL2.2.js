59016acaae635947e807e939b997b94a2f19a317
Svetlana180 <lana.kolcova@gmail.com>
2021-10-22 21:52:20 +0300

    solution task 2 fix

--- HWL2.2.js
+++ HWL2.2.js
@@ -1,12 +1,16 @@
-const vovelsCounter = (str) => {
-    const vovels = ['a', 'e', 'i', 'o', 'u']
-    let counter = 0
-    for(letter of str){
-        if(vovels.includes(letter)){
-            counter = counter + 1
-        }
-    }
-    return counter
-}
+const phones = [
+    "067 734 4343",
+    "094 643 7432",
+    "083 jfvj 4554",
+    "Anton0938 884",
+    "0437348348"
+];
 
+phones.forEach(phone =>
+    console.log("validatePhoneNymber(" + phone + ") -> " + validatePhoneNymber(phone)));
+
+function validatePhoneNymber(phone) {
+let isValid = Array.from(phone).every(symbol => !isNaN(symbol));
+return isValid;
+}
 console.log("Finish");
